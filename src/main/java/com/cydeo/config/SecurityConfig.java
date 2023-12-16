f62857e731d7126668a9e1bb39c558e74b338da7
Betul Zora <zorabtl@gmail.com>
2023-12-16 11:38:21 -0600

    Worked on authorizations and landing pages and SecurityConfig

---
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.cydeo.config;
+
+
+import com.cydeo.service.SecurityService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+
+    private final SecurityService securityService;
+    private final AuthSuccessHandler authSuccessHandler;
+
+    public SecurityConfig(SecurityService securityService, AuthSuccessHandler authSuccessHandler) {
+        this.securityService = securityService;
+        this.authSuccessHandler = authSuccessHandler;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests()
+                .antMatchers("/account/**").hasAuthority("Admin")
+                .antMatchers("/transaction/**").hasAnyAuthority("Admin","Cashier")
+                .antMatchers("/", "/login")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .successHandler(authSuccessHandler)
+                .failureUrl("/login?error=true")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds(300)
+                .key("bankapp")
+                .userDetailsService(securityService)
+                .and().build();
+    }
+
+}
